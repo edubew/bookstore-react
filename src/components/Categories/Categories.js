fe1d74777f67be920cd5e389819fc1206cc15a87
edubew <edubew98@gmail.com>
2023-01-21 07:06:49 +0300

    Create the Categories component

---
+++ src/components/Categories/Categories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './categories.css';
+
+const Categories = () => (
+  <div>
+    <button className="categories__btn" type="submit">
+      Check Status
+    </button>
+  </div>
+);
+
+export default Categories;
