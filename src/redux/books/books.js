7a2d14b6b745b1dafe726525b5d64dfcbfb37bb0
edubew <edubew98@gmail.com>
2023-01-27 11:04:47 +0300

    Add an array of books to the initial state of books reducer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,29 +1,60 @@
-const ADD_BOOK = 'bookstore-app-react/books/InputBook';
+import { v4 as uuidv4 } from 'uuid';
+
+const ADD_BOOK = 'bookstore-app-react/books/inputBook';
 const REMOVE_BOOK = 'bookstore-app-react/books/removeBook';
 
 const initialState = {
-  books: [],
+  books: [
+    {
+      id: uuidv4(),
+      genre: 'Action',
+      title: 'The Hunger Games',
+      author: 'Suzanne Collins',
+    },
+    {
+      id: uuidv4(),
+      genre: 'Science Fiction',
+      title: 'Dune',
+      author: 'Frank Herbert',
+    },
+    {
+      id: uuidv4(),
+      genre: 'Economy',
+      title: 'Capital in the Twenty-First Century',
+      author: 'Suzanne Collins',
+    },
+  ],
 };
 
 // Add Reducers
 export default function booksReducer(state = initialState, action) {
   switch (action.type) {
-    case ADD_BOOK:
-      return { ...state };
+    case ADD_BOOK: {
+      const newBook = [...state.books, action.payload];
+      return { ...state.books, books: newBook };
+    }
     case REMOVE_BOOK:
       return {
         ...state,
-        items: state.items.filter((item) => item.id !== action.id),
+        books: state.books.filter((book) => book.id !== action.payload.id),
       };
     default:
       return state;
   }
 }
 
-export const InputBook = () => ({
+export const inputBook = (title, author) => ({
   type: ADD_BOOK,
+  payload: {
+    title,
+    author,
+    id: uuidv4(),
+  },
 });
 
-export const removeBook = () => ({
+export const removeBook = (id) => ({
   type: REMOVE_BOOK,
+  payload: {
+    id,
+  },
 });
