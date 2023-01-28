430471069e32d8ca6ffff056b074c3ca3e1c8617
edubew <edubew98@gmail.com>
2023-01-28 09:18:43 +0300

    Dispatch an action to pass API response and update the state

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,24 +1,40 @@
-const ADD_BOOK = 'bookstore-app-react/books/InputBook';
-const REMOVE_BOOK = 'bookstore-app-react/books/removeBook';
+import axios from "axios";
 
-const initialState = {
-  books: [],
-};
+const BOOKS_URL =
+  "https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/R9iogCYmIf2eEINhPNCJ/books";
+
+const ADD_BOOK = "bookstore-react/books/InputBook";
+const REMOVE_BOOK = "bookstore-react/books/removeBook";
+const FETCH_BOOKS = "bookstore-react/books/fetchBooks";
+
+const initialState = [];
 
 // Add Reducers
 export default function booksReducer(state = initialState, action) {
   switch (action.type) {
     case ADD_BOOK:
-      return { ...state };
+      return action.payload;
+    case FETCH_BOOKS:
+      return action.payload;
     case REMOVE_BOOK:
-      return {
-        ...state,
-        items: state.items.filter((item) => item.id !== action.id),
-      };
+      return action.payload;
     default:
       return state;
   }
 }
+
+export const fetchBooks = () => async (dispatch) => {
+  try {
+    const response = await axios.get(BOOKS_URL);
+    dispatch({
+      type: FETCH_BOOKS,
+      payload: response.data,
+    });
+    return response.data;
+  } catch (err) {
+    return err.message;
+  }
+};
 
 export const InputBook = () => ({
   type: ADD_BOOK,
